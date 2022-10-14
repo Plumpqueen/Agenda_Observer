d408f11bc2df8c336b52666b66fa0a8d55dfee56
Mariam EL ALLALI <elallalimariam@gmail.com>
2022-10-14 16:23:42 +0200

    :tada: feat observer

--- dist/src/main.js
+++ dist/src/main.js
@@ -30,18 +30,72 @@ class Cleaner extends Person {
     }
 }
 class Classroom {
-    constructor(name, place) {
+    constructor(name, place, state) {
+        this.observers = [];
         this.name = name;
         this.place = place;
+        this.state = state;
+    }
+    attach(observer) {
+        const isExist = this.observers.includes(observer);
+        if (isExist) {
+            return console.log('Subject: Observer has been attached already.');
+        }
+        console.log('Subject: Attached an observer.');
+        this.observers.push(observer);
+    }
+    detach(observer) {
+        const observerIndex = this.observers.indexOf(observer);
+        if (observerIndex === -1) {
+            return console.log('Subject: Nonexistent observer.');
+        }
+        this.observers.splice(observerIndex, 1);
+        console.log('Subject: Detached an observer.');
+    }
+    notify() {
+        console.log('Subject: Notifying observers...');
+        for (const observer of this.observers) {
+            observer.update(this);
+        }
+    }
+    someBusinessLogic() {
+        console.log('\nCheck if this Classroom is available');
+        if (this.state === true) {
+            this.state = false;
+        }
+        else {
+            this.state = true;
+        }
+        console.log(`Subject: My state has just changed to: ${this.state}`);
+        this.notify();
+    }
+}
+class ConcreteObserverA {
+    update(subject) {
+        if (subject instanceof Classroom && subject.state === false) {
+            console.log('This classroom is already taken ! Choose another one');
+        }
+    }
+}
+class ConcreteObserverB {
+    update(subject) {
+        if (subject instanceof Classroom && (subject.state === true)) {
+            console.log('Congratulations, the classroom has been booked');
+        }
     }
 }
 class Book {
+    constructor(date, subject, duration, classroom) {
+        this.subject = subject;
+        this.duration = duration;
+        this.classroom = classroom;
+    }
 }
 function main() {
     //*Classrooms declarations
-    const classroom1 = new Classroom("B114", 5);
-    const classroom2 = new Classroom("B214", 6);
-    const classroom3 = new Classroom("B314", 7);
+    const classroom1 = new Classroom("B114", 5, true);
+    const classroom2 = new Classroom("B214", 6, true);
+    const classroom3 = new Classroom("B314", 7, true);
     //* Students declarations
     const student1 = new Student("Jean", "DELAFONTAINE");
     const student2 = new Student("Pierre", "POLJAK");
@@ -49,6 +103,14 @@ function main() {
     const teacher1 = new Teacher("Henri", "DESMATHS");
     //*Employees declarations
     const cleaner1 = new Cleaner("Monsieur", "PROPRE", 345);
+    const observer1 = new ConcreteObserverA();
+    classroom1.attach(observer1);
+    const observer2 = new ConcreteObserverB();
+    classroom1.attach(observer2);
+    classroom1.someBusinessLogic();
+    classroom1.someBusinessLogic();
+    classroom1.detach(observer2);
+    classroom1.someBusinessLogic();
     console.log(cleaner1);
 }
 main();
--- src/main.ts
+++ src/main.ts
@@ -33,12 +33,70 @@ class Cleaner extends Person {
         return "clean";
     }
 }
-class Classroom{
+
+interface ClassroomInterface {
+    attach(observer: Observer): void;
+    detach(observer: Observer): void;
+    notify(): void;
+}
+interface Observer {
+    // Receive update from subject.
+    update(subject: ClassroomInterface): void;
+}
+
+class Classroom implements ClassroomInterface{
     name:string;
     place:number;
-    constructor(name:string, place:number){
+    state:boolean;
+    private observers: Observer[] = [];
+
+    constructor(name:string, place:number,state:boolean){
         this.name=name;
         this.place=place;
+        this.state=state;
+    }
+    public attach(observer: Observer): void {
+        this.observers.push(observer);
+    }
+
+    public detach(observer: Observer): void {
+        const observerIndex = this.observers.indexOf(observer);
+        this.observers.splice(observerIndex, 1);
+    }
+
+    public notify(): void {
+        for (const observer of this.observers) {
+            observer.update(this);
+        }
+    }
+
+    public bookAClass(): void {
+        console.log('\nChange classroom status');
+        let status:string="";
+        if(this.state === true) {
+            this.state = false;
+            status = "Not available";
+        }else{
+            this.state = true;
+            status = "Available"
+        }
+        console.log(`Classroom status has just been changed to: ${status}`);
+        this.notify();
+    }
+}
+
+class ConcreteObserverA implements Observer {
+    public update(subject: ClassroomInterface): void {
+        if (subject instanceof Classroom && subject.state === false) {
+            console.log('This classroom is already taken ! Choose another one');
+        }
+    }
+}
+class ConcreteObserverB implements Observer {
+    public update(subject: ClassroomInterface): void {
+        if (subject instanceof Classroom && (subject.state === true)) {
+            console.log('Congratulations, the classroom has been booked');
+        }
     }
 }
 class Book{
@@ -57,9 +115,9 @@ class Book{
 
 function main(): void {
     //*Classrooms declarations
-    const classroom1:Classroom = new Classroom("B114",5);
-    const classroom2:Classroom = new Classroom("B214",6);
-    const classroom3:Classroom = new Classroom("B314",7);
+    const classroom1:Classroom = new Classroom("B114",5,true);
+    const classroom2:Classroom = new Classroom("B214",6,true);
+    const classroom3:Classroom = new Classroom("B314",7,true);
 
     //* Students declarations
     const student1:Student = new Student("Jean","DELAFONTAINE");
@@ -71,7 +129,22 @@ function main(): void {
     //*Employees declarations
     const cleaner1:Cleaner = new Cleaner("Monsieur","PROPRE",345);
 
-    console.log(cleaner1);
+
+const observer1 = new ConcreteObserverA();
+classroom1.attach(observer1);
+
+const observer2 = new ConcreteObserverB();
+classroom1.attach(observer2);
+
+//*Call this function everytime we create a new book:Book
+classroom1.bookAClass();
+classroom1.bookAClass();
+
+classroom1.detach(observer2);
+
+classroom1.bookAClass();
+
+
 
 }
 
